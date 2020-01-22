c270fc34c07ddbff9d0e1e310abf83c6237a762e
sushma2605 <39223310+sushma2605@users.noreply.github.com>
2020-01-22 18:37:12 +0530

    Add files via upload

---
+++ employeeController.js
@@ -0,0 +1,70 @@
+const express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
+
+router.get('/', (req, res) => {
+    // res.json('sample text');
+    res.render("employee/addOrEdit", {
+        viewTitle: "Insert Employee"
+    });
+});
+   router.post('/', (req, res) => {
+       console.log(req.body);
+       insertRecord(req,res);
+   });
+
+   function insertRecord(req,res){
+       var employee  = new Employee();
+       employee.fullName = req.body.fullName;
+       employee.email = req.body.email;
+       employee.mobile = req.body.mobile;
+       employee.city = req.body.city;
+       employee.save((err,doc) => {
+           if(!err){
+               res.redirect('employee/list');
+           }else{
+               if(err.name == 'ValidationError'){
+                   handleValidationError(err, req.body);
+                   res.render("employee/addOrEdit", {
+                       viewTitle: "Insert Employee",
+                       employee: req.body
+                   })
+               }else{
+                console.log("Error: " + err);
+               }
+               
+           }
+       });
+   }
+
+   router.get('/list' , (req,res) => {
+        Employee.find((err, docs) => {
+           if(!err) {
+               res.render("employee/list",
+               {
+                   list: docs
+               });
+           }else{
+               console.log("Error : " + err);
+           }
+       });
+   });
+
+   function handleValidationError(err, body){
+      for(field in err.errors) 
+      {
+          switch (err.errors[field].path) {
+              case 'fullName' :
+                  body['fullNameError'] = err.errors[field].message;
+                  break;
+            case 'email':
+                body['emailError'] = err.errors[field].message;
+                break;
+               case 'default':
+                break;
+          }
+      }
+   }
+
+module.exports = router;
